ba9ec556de013209a1f4f0c03fc9df616582bacc
DrimLover <nikikikikiki7725@gmail.com>
2024-09-25 17:12:41 +0200

    pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Hello and ",
+  description: "Goodbye",
+};
 
 export default function Home() {
   return (
